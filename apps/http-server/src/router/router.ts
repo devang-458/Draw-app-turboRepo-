efd80f2a40484bc6d269a4ba0a2c9a47e0888488
Devang <deekayedm@gmail.com>
2025-07-10 17:16:07 +0530

    added whole project structure

--- apps/http-server/src/router/router.ts
+++ apps/http-server/src/router/router.ts
@@ -1,7 +1,28 @@
 import express, { Router } from "express";
+import jwt from "jsonwebtoken";
+import { CreateUserScehma } from "@repo/common/types";
+import { prismaClient } from "@repo/db/client";
 
 const router: Router = express.Router();
 
+router.post("/signup", async (req, res) => {
+  const parsedData = CreateUserScehma.safeParse(req.body);
+  if (!parsedData.success) {
+    console.log(parsedData.error);
+    res.json({
+      message: "Incorrect Input",
+    });
+  }
+
+  try {
+    const user = await prismaClient.
+  } catch (err) {
+    console.log("");
+  }
+});
+
 router.post("/signin", (req, res) => {});
+
+router.post("/room", (req, res) => {});
 
 export default router;
--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-export const CreateUserSchema = z.object({
+export const CreateUserScehma = z.object({
   username: z.string().min(3).max(20),
   password: z.string(),
   name: z.string(),
@@ -10,7 +10,6 @@ export const SigninSchema = z.object({
   username: z.string().min(3).max(20),
   password: z.string(),
 });
-
 export const CreateRoomSchema = z.object({
   name: z.string().min(3).max(20),
 });
